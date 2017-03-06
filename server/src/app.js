ecfcdffa96675b2a992a4c8b0ef3575c98ae3d96
Mihail Tratsiuk <mihail.tratsiuk@gmail.com>
2017-03-07 00:56:19 +0300

    Integrate client build with server

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const path = require('path')
 
 const logger = require('./utils/logger')
 const routes = require('./routes')
 const errorHandler = require('./middlewares/error-handling')
+
+const STATIC_PATH = path.resolve(__dirname, `${process.env.CONTRAST_SERVER_STATIC_PATH}`)
 
 const app = express()
 
@@ -12,6 +15,15 @@ app.use(bodyParser.json())
 app.use(cookieParser())
 
 app.use('/api', routes)
+
+app.use('/static', express.static(`${STATIC_PATH}/static`, {
+  maxAge: '1y'
+}))
+
+app.get('*', (req, res) => {
+  res.set('Cache-Control', 'no-cache, no-store, must-revalidate')
+  res.sendFile(`${STATIC_PATH}/index.html`)
+})
 
 app.use(errorHandler({ logger }))
 
