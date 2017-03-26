4b67f1ed1d7f81e6993d5f9db8806f80ccddbe17
Mihail Tratsiuk <mihail.tratsiuk@gmail.com>
2017-03-27 00:15:06 +0300

    Update storage

--- client/src/reducers/user.js
+++ client/src/reducers/user.js
@@ -1,14 +1,6 @@
-import storage from 'utils/storage'
+import { get } from 'utils/storage'
 
-const getUserFromStorage = () => {
-  try {
-    return JSON.parse(storage.getItem('user'))
-  } catch (error) {
-    return null
-  }
-}
-
-const user = (state = getUserFromStorage(), action) => {
+const user = (state = get('user'), action) => {
   switch (action.type) {
     case 'USER.LOGIN':
       return action.payload
--- client/src/utils/storage.js
+++ client/src/utils/storage.js
@@ -18,4 +18,18 @@ const storage = window.localStorage || {
   }
 }
 
-export default storage
+export const get = key => {
+  try {
+    return JSON.parse(storage.getItem(key))
+  } catch (error) {
+    return null
+  }
+}
+
+export const set = (key, value) => {
+  storage.setItem(key, JSON.stringify(value))
+}
+
+export const remove = key => storage.removeItem(key)
+
+export const clear = () => storage.clear()
