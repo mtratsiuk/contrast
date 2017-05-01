72b9ff28863d6fb205b652a000471a36fb1c633f
Mihail Tratsiuk <mihail.tratsiuk@gmail.com>
2017-05-02 01:33:29 +0300

    Fix currency codes: take uniq only

--- bin/build-currency-list.py
+++ bin/build-currency-list.py
@@ -10,7 +10,7 @@ def build_currency(res_path):
   with urllib.request.urlopen(currency_url) as response, open(res_path, 'w') as res_file:
     xml = response.read()
     root = ET.fromstring(xml)
-    res_json = json.dump([entry.text for entry in root.iter('Ccy')], res_file, indent=2)
+    res_json = json.dump(list(set([entry.text for entry in root.iter('Ccy')])), res_file, indent=2)
 
 if __name__ == '__main__':
   import sys
