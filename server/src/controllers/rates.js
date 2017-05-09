9f3675d1735d5e1497841c4e2cb3d65290faa980
Mihail Tratsiuk <mihail.tratsiuk@gmail.com>
2017-05-09 17:11:52 +0300

    Add cache-control

--- server/src/controllers/rates.js
+++ server/src/controllers/rates.js
@@ -1,6 +1,7 @@
 module.exports = ({ rates }) => {
   const get = async (req, res, next) => {
     try {
+      res.set('Cache-Control', 'max-age=3600')
       res.json(await rates.get())
     } catch (error) {
       next(error)
