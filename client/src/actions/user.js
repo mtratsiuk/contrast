c1983eb1037fd69eecd2454b818e5673e46a2a3d
Mihail Tratsiuk <mihail.tratsiuk@gmail.com>
2017-05-09 19:58:38 +0300

    Update user service

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -1,29 +1,13 @@
-import contrast from 'services/contrast'
 import user from 'services/user'
 
-const _login = (data, dispatch) => {
-  user.login(data)
-  return dispatch({
-    type: 'USER.LOGIN',
-    payload: data
-  })
-}
-
 export const signup = (name, password) => async dispatch => {
-  let { data } = await contrast.signup(name, password)
-  return _login(data, dispatch)
+  return user.signup(name, password)
 }
 
 export const login = (name, password) => async dispatch => {
-  let { data } = await contrast.login(name, password)
-  return _login(data, dispatch)
+  return user.login(name, password)
 }
 
 export const logout = () => async dispatch => {
-  try {
-    await contrast.logout()
-  } finally {
-    user.logout()
-    dispatch({ type: 'USER.LOGOUT' })
-  }
+  return user.logout()
 }
--- client/src/services/user.js
+++ client/src/services/user.js
@@ -1,17 +1,39 @@
 import EventEmitter from 'utils/event-emitter'
 import { get, set, remove } from 'utils/storage'
+import http from 'shared/http'
+import store from 'store'
 
+const BASE_URL = '/api'
 const USER_KEY = 'user'
 
 class UserService extends EventEmitter {
-  login (data) {
+  _login (data) {
     set(USER_KEY, data)
     this._emit('login', data)
+    return store.dispatch({
+      type: 'USER.LOGIN',
+      payload: data
+    })
   }
 
-  logout () {
-    remove(USER_KEY)
-    this._emit('logout')
+  async signup (name, password) {
+    let { data } = await http.post(`${BASE_URL}/signup`, { name, password })
+    return this._login(data)
+  }
+
+  async login (name, password) {
+    let { data } = await http.post(`${BASE_URL}/login`, { name, password })
+    return this._login(data)
+  }
+
+  async logout () {
+    try {
+      await http.post(`${BASE_URL}/logout`)
+    } finally {
+      remove(USER_KEY)
+      this._emit('logout')
+      store.dispatch({ type: 'USER.LOGOUT' })
+    }
   }
 
   isLoggedIn () {
